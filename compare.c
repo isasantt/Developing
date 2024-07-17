1af5aaf548b74c4a048380a78a52ef00d2efa46e
Isabela Santiago <isadelga2009@gmail.com>
2024-07-17 12:47:55 -0300

    compare.c

--- compare.c
+++ compare.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
+//teaching the compiler about get_string
 #include <stdio.h>
+//teaching the compiler about standard input/output
 int main (void) {
     int x = get_int("What's x? ");
     int y = get_int("What's y? ");
